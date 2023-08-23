232442ed8734eb6dcdbe822448dc9678cce14ee2
prudhvi1836 <prudhvi.chittiboina18@gmail.com>
2023-08-23 11:28:31 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# First
+# PracticeAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.0.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ImageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
